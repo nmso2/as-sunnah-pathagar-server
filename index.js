f414cf8333a198abd66cb75c4033e3aa6f444a8d
nmso2 <nmsubho@gmail.com>
2021-11-26 23:57:09 +0600

    added delete method

--- index.js
+++ index.js
@@ -118,6 +118,14 @@ async function run() {
             res.send(requestedBook);
         });
 
+        // DELETE API
+        app.delete('/requestedBook/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await requestedBookCollection.deleteOne(query);
+            res.json(result);
+        })
+
     }
     finally {
         //   await client.close();
